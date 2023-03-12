300c9fea4db6b479ed3ccb47fb14f1fa5b654cf4
shruti <rajeshwaris5552@gmail.com>
2023-03-12 20:30:35 +0530

    candidate and job page code updated

--- src/app/components/candidate/candidate.component.ts
+++ src/app/components/candidate/candidate.component.ts
@@ -1,20 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { candidateService } from 'src/app/services/candidate.service';
 import { map } from 'rxjs';
 import { candidate } from 'src/app/models/candidate';
+declare var dataTableInit: any;
+
 @Component({
   selector: 'app-candidate',
   templateUrl: './candidate.component.html',
   styleUrls: ['./candidate.component.scss']
 })
-export class CandidateComponent {
+export class CandidateComponent implements OnInit{
+  
 Candidate:candidate[];
+cols:any;
+selectedProducts:candidate[];
 
   constructor(
     public candidateService:candidateService
   ){ }
 
   ngOnInit() {
+    this.cols = [
+      { field: 'name', header: 'name', customExportHeader: 'NAME' },
+      { field: 'phone', header: 'phone' },
+      { field: 'email', header: 'email' },
+      { field: 'tech', header: 'tech' },
+      { field: 'visa', header: 'visa' },
+      { field: 'rate', header: 'rate' },
+      { field: 'date', header: 'date' },
+
+  ];
+
     this.candidateService.getAll().snapshotChanges().pipe(
       map(changes =>
         changes.map(c =>
